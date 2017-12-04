c8c3268cdd26052979ee902416326652b0643408
Pulkit Verma <pulkitver1991@gmail.com>
2017-12-04 18:02:11 +0530

    Modbus Code Added, Testing remaining

--- project/lib/mod_gen.py
+++ project/lib/mod_gen.py
@@ -23,6 +23,7 @@ class ModbusNode(object):
 		self.all_endianness = []
 		self.all_device_address = []
 		self.all_retry_counts = []
+		self.all_files = []
 		# File containing the modbus responses of the read addresses 
 		self.mod_data_file = ""
 		# File location and name containing the modbus responses of the read addresses 
@@ -66,6 +67,7 @@ class ModbusNode(object):
 			self.all_endianness.append(endianness)
 			self.all_device_address.append(device_address)
 			self.all_retry_counts.append(retry_counts)
+			self.all_files = ports
 		# print (self.all_addresses)
 		# print (self.all_length)
 		# print (self.all_datatype)
@@ -129,33 +131,40 @@ class ModbusNode(object):
 
 
 	def modRead(self, rjson, port_num):
+		port_data = []
 		for file_count in range(len(self.all_device_address)):
 			if self.all_addresses[file_count] == None:
 				continue
 			try:
+				file_result = []
 				instrument = self.init_modbus(rjson.mod_port_addr[port_num], self.all_device_address[file_count] ,\
 											  rjson.mod_baudrate[port_num], rjson.mod_databits[port_num],\
 											  rjson.mod_parity[port_num], rjson.mod_stopbits[port_num],\
 											  rjson.mod_poll_timeout[port_num])
 				instrument.debug = False
 				for addr_in_files in range(len(self.all_addresses[file_count])):
+					addr_data = []
 					for len_count in range(self.all_length[file_count][addr_in_files]):
 						retry_counter = 0
 						while retry_counter < self.all_retry_counts[file_count]:
 							try:
 								bytelength = self.getByteLength(self.all_datatype[file_count][addr_in_files])
-								print (self.selectReadFunc(instrument, \
+								result =  (self.selectReadFunc(instrument, \
 									  (self.all_addresses[file_count][addr_in_files] + len_count * bytelength), \
 									   self.all_datatype[file_count][addr_in_files], \
 									   self.getEndianness(self.all_endianness[file_count])))
+								addr_data.append(result)
 								break
 							except:
 								linear_modbus.info("Error in reading address " + str(self.all_addresses[file_count][addr_in_files]) + \
 										   ". Retrying...")
 								if retry_counter == self.all_retry_counts[file_count]:
+									result = 'error'
+									addr_data.append(result)
 									raise
 								retry_counter += 1
-					time.sleep(self.cons_read_reg_timeout)		
+					time.sleep(self.cons_read_reg_timeout)
+					file_result.append(addr_data)		
 			except ValueError as e:
 				linear_modbus.error("Value Error : ({0})".format(e))
 			except TypeError as e:
@@ -164,13 +173,40 @@ class ModbusNode(object):
 				linear_modbus.error("IO Error : ({0})".format(e))
 			except:
 				linear_modbus.error("Failure due to other errors")
-		return "hello"
+			port_data.append(file_result)
+		return port_data
+
+	def ModFileConversion(self, data, rjson):
+			row = ""
+			for files in range(len(data)):
+				#add 1st line in output file	
+				row += "ADDRMODBUS" + ";" + str(self.all_device_address[files]) + "\n"
+				# add 2nd line in output file
+				row += "TypeMODBUS" + ";" + self.all_files[files] + "\n"
+				total_count = 0
+				# adding 3rd line in output  file
+				counts_val = []
+				for counts in range(len(self.all_length[files])):
+					total_count = total_count + self.all_length[files][counts]
+				for val in range(total_count):
+					counts_val.append(val)
+				addr_count = ';'.join(str(e) for e in counts_val)
+				row += str(total_count) + ";" + addr_count + "\n"
+				# adding 4th line in output file
+				results = []
+				for res in range(len(data[files])):
+					for length in range(len(data[files][res])):
+						results.append(data[files][res][length])
+
+				addr_results = ';'.join(str(e) for e in results)
+				row += time.strftime("%d/%m/%Y-%H:%M:%S", time.localtime()) + ";" + addr_results + "\n" 
+			return row
 
 	def ModCreateFile(self, rjson, port_num):
 		# read the Modbus data for the specified addresses
 		read_value = self.modRead(rjson, port_num)
 		# convert the data read from modbus in a particular format
-		# value = self.ModFileConversion(read_value, rjson)
+		value = self.ModFileConversion(read_value, rjson)
 		# Increment the file counter, when file is sent
 		self.mod_data_file_timestamp = time.strftime("%Y%m%d-%H%M%S")
 
@@ -179,4 +215,4 @@ class ModbusNode(object):
 							+ self.mod_data_file_timestamp + self.mod_data_file_ext
 		# write the data into the file
 		with open(self.mod_data_file, 'w') as f: 
-			f.write(read_value)	
+			f.write(value)	
